0b0a0d323b86bade18130c1e3e5027f1546058a4
Matthew Bishop <bishop@atmos.colostate.edu>
2012-04-26 23:34:19 -0600

    plane generating function

--- terrain.js
+++ terrain.js
@@ -1,26 +1,71 @@
-/*
- This example demonstrates how to define and mutate geometry
+(function() {
+    B = {};
 
- Lindsay S. Kay,
- lindsay.kay@xeolabs.com
+    /* Generate a geometry node of a plane divided into n rows and m columns,
+     * each a unit distance apart, using indexed triangles to render.
+     */
+    B.plane = function(nx, ny) {
+        var nx1 = nx + 1, ny1 = ny + 1;
+        var pos  = new Array(nx1 * ny1 * 3);
+        var norm = new Array(nx1 * ny1 * 3);
+        var ind  = new Array(nx * ny * 6);
+        var x, y, i, rowA, rowB;
 
- https://github.com/xeolabs/scenejs/wiki/geometry
- */
+        i = 0;
+        for (y = 0; y <= ny; y++) {
+            for (x = 0; x <= nx; x++) {
+                pos[i    ] = x;
+                pos[i + 1] = y;
+                pos[i + 2] = -0.1;
 
-var geom = {
-    type: "geometry",
-    id: "my-geometry",
-    primitive: "triangles",
+                norm[i    ] = 0;
+                norm[i + 1] = 0;
+                norm[i + 2] = 1;
 
-    positions: [ 0, 0, 0,  1, 0, 0,  0, 1, 0,  1, 1, 0 ],
+                i += 3;
+            }
+        }
 
-    normals: [ 0, 0, 1,  0, 0, 1,  0, 0, 1,  0, 0, 1 ],
+        i = 0;
+        for (y = 0; y < ny; y++) {
+            rowA = y * nx1;
+            rowB = rowA + nx1;
+            for (x = 0; x < nx; x++) {
+                ind[i    ] = x + rowA;
+                ind[i + 1] = x + rowA + 1;
+                ind[i + 2] = x + rowB;
 
-    colors: [ 0.35, 0.60, 0.88, 1.0,  0.17, 0.61, 0.13, 1.0,
-              0.48, 0.31, 0.14, 1.0,  0.93, 0.86, 0.98, 1.0 ],
+                ind[i + 3] = x + rowB;
+                ind[i + 4] = x + rowA + 1;
+                ind[i + 5] = x + rowB + 1;
 
-    indices: [ 0, 1, 2,  2, 1, 3 ]
-};
+                i += 6;
+            }
+        }
+
+        return { type: "geometry", primitive: "triangles",
+                 positions: pos, normals: norm, indices: ind };
+    }
+})();
+
+var COLORS = [
+    [0.35, 0.60, 0.88, 1.0], // water
+    [0.17, 0.61, 0.13, 1.0], // grass
+    [0.48, 0.31, 0.14, 1.0], // dirt
+    [0.93, 0.86, 0.98, 1.0], // snow
+    [0.40, 0.40, 0.45, 1.0]  // gray
+];
+
+var geom = B.plane(4, 4); 
+geom.colors = [];
+(function() {
+    for (var y = 0; y <= 4; y++) {
+        for (var x = 0; x <= 4; x++) {
+            var i = Math.floor(Math.random() * COLORS.length);
+            geom.colors = geom.colors.concat(COLORS[i]);
+        }
+    }
+})();
 
 SceneJS.createScene({
     type: "scene",
@@ -35,8 +80,8 @@ SceneJS.createScene({
     nodes: [
         {
             type: "lookAt",
-            eye: { x: 2, y: -6, z: 3 },
-            look: { x: 0.5, y: 0.5, z: 0 },
+            eye: { x: 5, y: -18, z: 6 },
+            look: { x: 0, y: 0, z: 0 },
             up: { x: 0, y: 0, z: 1 },
 
             nodes: [
